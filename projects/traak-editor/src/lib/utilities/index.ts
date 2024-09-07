7f455f294b551d6da459d64a2cda1fa4a63ddfa5
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-09-07 17:00:54 +0200

    chore(lib): add comments to utilities

--- projects/traak-editor/src/lib/utilities/index.ts
+++ projects/traak-editor/src/lib/utilities/index.ts
@@ -8,26 +8,46 @@ function getResolvedPos(view: EditorView) {
   return $from;
 }
 
+/**
+ * returns the type of the current node.
+ * @param view
+ */
 export function getNodeType(view: EditorView): string {
   const $nodePos = getResolvedPos(view);
   return $nodePos.parent.type.name;
 }
 
+/**
+ * returns the current node.
+ * @param view
+ */
 export function getNode(view: EditorView) {
   const $nodePos = getResolvedPos(view);
   return $nodePos.parent;
 }
 
+/**
+ * returns true if the node is empty. False otherwise.
+ * @param view
+ */
 export function isNodeEmpty(view: EditorView) {
   const $nodePos = getResolvedPos(view);
   return $nodePos.parent.content.size === 0;
 }
 
+/**
+ * returns true if the cursor is at the start of the current node.
+ * @param view
+ */
 export function isCursorAtStartOfNode(view: EditorView) {
   const $nodePos = getResolvedPos(view);
-  return $nodePos.pos === $nodePos.before() - 1;
+  return $nodePos.pos === $nodePos.before() + 1;
 }
 
+/**
+ * dispatches a transaction that moves the cursor to the end of the current node.
+ * @param view
+ */
 export function moveCursorToEndOfNode(view: EditorView) {
   const { state } = view;
   const { tr } = state;
@@ -35,8 +55,9 @@ export function moveCursorToEndOfNode(view: EditorView) {
   tr.setSelection(
     TextSelection.create(
       tr.doc,
-      $nodePos.parent.nodeSize + 1,
-      $nodePos.parent.nodeSize + 1,
+      $nodePos.node().nodeSize + 1,
+      $nodePos.node().nodeSize + 1,
     ),
   );
+  view.dispatch(tr);
 }
