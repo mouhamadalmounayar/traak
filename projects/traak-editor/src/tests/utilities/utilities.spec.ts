a4ddaa1ab734503eebb1028c6b7eab7f3df5a0d8
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-09-07 16:59:13 +0200

    test(lib): add corresponding tests for utility methods

---
+++ projects/traak-editor/src/tests/utilities/utilities.spec.ts
@@ -0,0 +1,81 @@
+import { builders, eq } from 'prosemirror-test-builder';
+import { traakSchema } from '../../lib/builtins/schemas';
+import { createView, createState } from '../__utils__';
+import ist from 'ist';
+import {
+  getNodeType,
+  isCursorAtStartOfNode,
+  isNodeEmpty,
+  moveCursorToEndOfNode,
+} from '../../lib/utilities';
+
+describe('Node utilities', () => {
+  const traakBuilders = builders(traakSchema);
+  describe('getNodeType', () => {
+    it('should return line', () => {
+      const doc = traakBuilders.doc(traakBuilders.line('Hello from<a> traak'));
+      const view = createView(createState(doc));
+      const result = getNodeType(view);
+      expect(result).toEqual('line');
+    });
+    it('should return list-item', () => {
+      const doc = traakBuilders.doc(
+        traakBuilders.bullet_list(
+          traakBuilders.list_item('Hello <a> from traak'),
+        ),
+      );
+      const view = createView(createState(doc));
+      const result = getNodeType(view);
+      expect(result).toEqual('list_item');
+    });
+    it('should return doc_title', () => {
+      const doc = traakBuilders.doc(traakBuilders.doc_title('Page<a> title'));
+      const view = createView(createState(doc));
+      const result = getNodeType(view);
+      expect(result).toEqual('doc_title');
+    });
+  });
+  describe('isNodeEmpty', () => {
+    it('should return true for empty node', async () => {
+      const doc = traakBuilders.doc(traakBuilders.line());
+      const view = createView(createState(doc));
+      const result = isNodeEmpty(view);
+      expect(result).toEqual(true);
+    });
+    it('should return false for a non empty node', async () => {
+      const doc = traakBuilders.doc(traakBuilders.line('Hello from traak<a>'));
+      const view = createView(createState(doc));
+      const result = isNodeEmpty(view);
+      expect(result).toEqual(false);
+    });
+  });
+  describe('isCursorAtStartOfNode', () => {
+    it('should return true for cursor at starting of node', async () => {
+      const doc = traakBuilders.doc(traakBuilders.line('<a>Hello from traak'));
+      const view = createView(createState(doc));
+      const result = isCursorAtStartOfNode(view);
+      expect(result).toEqual(true);
+    });
+    it('should return false for cursor at middle of node', async () => {
+      const doc = traakBuilders.doc(traakBuilders.line('Hello <a> from traak'));
+      const view = createView(createState(doc));
+      const result = isCursorAtStartOfNode(view);
+      expect(result).toEqual(false);
+    });
+  });
+  describe('moveCursorToEndOfNode', () => {
+    it('should move cursor to the end of the current node', async () => {
+      const doc = traakBuilders.doc(
+        traakBuilders.line('Hello<a>from traak'),
+        traakBuilders.line(''),
+      );
+      const view = createView(createState(doc));
+      const expectedDoc = traakBuilders.doc(
+        traakBuilders.line('Hellofrom traak<a>'),
+        traakBuilders.line(''),
+      );
+      moveCursorToEndOfNode(view);
+      ist(view.state.doc, expectedDoc, eq);
+    });
+  });
+});
