b1cce76e9b029bb0e9a243d694179f14a5b59e53
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-09-30 23:47:25 +0200

    refactor(builtins): turn utilities into a class

--- projects/traak-editor/src/lib/utilities/index.ts
+++ projects/traak-editor/src/lib/utilities/index.ts
@@ -1,97 +1,102 @@
 import { EditorView } from 'prosemirror-view';
 import { TextSelection } from 'prosemirror-state';
-import { NodeType } from 'prosemirror-model';
+import { NodeType, ResolvedPos } from 'prosemirror-model';
 
-function getResolvedPos(view: EditorView) {
-  const { state } = view;
-  const { selection } = state;
-  const { $from } = selection;
-  return $from;
-}
+export class TraakManager {
+  constructor(private view: EditorView) {
+    this.view = view;
+  }
 
-/**
- * returns the type of the current node.
- * @param view
- */
-export function getNodeType(view: EditorView): string {
-  const $nodePos = getResolvedPos(view);
-  return $nodePos.parent.type.name;
-}
+  updateView(view: EditorView): void {
+    this.view = view;
+  }
 
-/**
- * returns the current node.
- * @param view
- */
-export function getNode(view: EditorView) {
-  const $nodePos = getResolvedPos(view);
-  return $nodePos.parent;
-}
+  private getResolvedPos(): ResolvedPos {
+    const { state } = this.view;
+    const { selection } = state;
+    const { $from } = selection;
+    return $from;
+  }
 
-/**
- * returns the text content of the current node.
- * @param view
- */
-export function getTextContent(view: EditorView): string {
-  const $nodePos = getResolvedPos(view);
-  return $nodePos.parent.textContent;
-}
+  getNodeType(): string {
+    const $nodePos = this.getResolvedPos();
+    return $nodePos.parent.type.name;
+  }
 
-/**
- * returns true if the node is empty. False otherwise.
- * @param view
- */
-export function isNodeEmpty(view: EditorView) {
-  const $nodePos = getResolvedPos(view);
-  return $nodePos.parent.content.size === 0;
-}
+  /**
+   * Returns the current node.
+   */
+  getNode() {
+    const $nodePos = this.getResolvedPos();
+    return $nodePos.parent;
+  }
 
-/**
- * returns true if the cursor is at the start of the current node.
- * @param view
- */
-export function isCursorAtStartOfNode(view: EditorView) {
-  const $nodePos = getResolvedPos(view);
-  return $nodePos.pos === $nodePos.before() + 1;
-}
+  /**
+   * Returns the text content of the current node.
+   */
+  getTextContent(): string {
+    const $nodePos = this.getResolvedPos();
+    return $nodePos.parent.textContent;
+  }
 
-/**
- * dispatches a transaction that moves the cursor to the end of the current node.
- * @param view
- */
-export function moveCursorToEndOfNode(view: EditorView) {
-  const { state } = view;
-  const { tr } = state;
-  const $nodePos = getResolvedPos(view);
-  tr.setSelection(
-    TextSelection.create(
-      tr.doc,
-      $nodePos.node().nodeSize + 1,
-      $nodePos.node().nodeSize + 1,
-    ),
-  );
-  view.dispatch(tr);
-}
+  /**
+   * Returns true if the node is empty, false otherwise.
+   */
+  isNodeEmpty(): boolean {
+    const $nodePos = this.getResolvedPos();
+    return $nodePos.parent.content.size === 0;
+  }
 
-/**
- * returns the coordinates of the current position in the document.
- * @param view
- */
-export function getSelectionCoords(view: EditorView) {
-  const $nodePos = getResolvedPos(view);
-  return view.coordsAtPos($nodePos.pos);
-}
+  /**
+   * Returns true if the cursor is at the start of the current node.
+   */
+  isCursorAtStartOfNode(): boolean {
+    const $nodePos = this.getResolvedPos();
+    return $nodePos.pos === $nodePos.before() + 1;
+  }
 
-/**
- * Adds a node at the current selection.
- * @param view
- * @param node
- */
-export function addNode(view: EditorView, node: NodeType) {
-  const $nodePos = getResolvedPos(view);
-  const { state } = view;
-  const { tr } = state;
-  // improvement : handle the content.
-  const nodeToInsert = node.create();
-  tr.insert($nodePos.pos, nodeToInsert);
-  view.dispatch(tr);
+  /**
+   * Dispatches a transaction that moves the cursor to the end of the current node.
+   */
+  moveCursorToEndOfNode() {
+    if (!this.view) {
+      throw new Error('EditorView is not set');
+    }
+    const { state } = this.view;
+    const { tr } = state;
+    const $nodePos = this.getResolvedPos();
+
+    const endPos = $nodePos.start() + $nodePos.parent.nodeSize;
+    tr.setSelection(TextSelection.create(tr.doc, endPos, endPos));
+    this.view.dispatch(tr);
+  }
+
+  /**
+   * Returns the coordinates of the current position in the document.
+   */
+  getSelectionCoords() {
+    if (!this.view) {
+      throw new Error('EditorView is not set');
+    }
+    const $nodePos = this.getResolvedPos();
+    return this.view.coordsAtPos($nodePos.pos);
+  }
+
+  /**
+   * Adds a node at the current selection.
+   */
+  addNode(node: NodeType) {
+    if (!this.view) {
+      throw new Error('EditorView is not set');
+    }
+    const $nodePos = this.getResolvedPos();
+    const { state } = this.view;
+    const { tr } = state;
+
+    // Create a new instance of the node
+    const nodeToInsert = node.create();
+
+    tr.insert($nodePos.pos, nodeToInsert);
+    this.view.dispatch(tr);
+  }
 }
--- projects/traak-editor/src/tests/utilities/utilities.spec.ts
+++ projects/traak-editor/src/tests/utilities/utilities.spec.ts
@@ -2,21 +2,15 @@ import { builders, eq } from 'prosemirror-test-builder';
 import { traakSchema } from '../../lib/builtins/schemas';
 import { createView, createState } from '../__utils__';
 import ist from 'ist';
-import {
-  addNode,
-  getNodeType,
-  isCursorAtStartOfNode,
-  isNodeEmpty,
-  moveCursorToEndOfNode,
-} from '../../lib/utilities';
-
+import { TraakManager } from '../../lib/utilities';
 describe('Node utilities', () => {
   const traakBuilders = builders(traakSchema);
   describe('getNodeType', () => {
     it('should return line', () => {
       const doc = traakBuilders.doc(traakBuilders.line('Hello from<a> traak'));
       const view = createView(createState(doc));
-      const result = getNodeType(view);
+      const manager = new TraakManager(view);
+      const result = manager.getNodeType();
       expect(result).toEqual('line');
     });
     it('should return list-item', () => {
@@ -26,13 +20,15 @@ describe('Node utilities', () => {
         ),
       );
       const view = createView(createState(doc));
-      const result = getNodeType(view);
+      const manager = new TraakManager(view);
+      const result = manager.getNodeType();
       expect(result).toEqual('list_item');
     });
     it('should return doc_title', () => {
       const doc = traakBuilders.doc(traakBuilders.doc_title('Page<a> title'));
       const view = createView(createState(doc));
-      const result = getNodeType(view);
+      const manager = new TraakManager(view);
+      const result = manager.getNodeType();
       expect(result).toEqual('doc_title');
     });
   });
@@ -40,13 +36,15 @@ describe('Node utilities', () => {
     it('should return true for empty node', async () => {
       const doc = traakBuilders.doc(traakBuilders.line());
       const view = createView(createState(doc));
-      const result = isNodeEmpty(view);
+      const manager = new TraakManager(view);
+      const result = manager.isNodeEmpty();
       expect(result).toEqual(true);
     });
     it('should return false for a non empty node', async () => {
       const doc = traakBuilders.doc(traakBuilders.line('Hello from traak<a>'));
       const view = createView(createState(doc));
-      const result = isNodeEmpty(view);
+      const manager = new TraakManager(view);
+      const result = manager.isNodeEmpty();
       expect(result).toEqual(false);
     });
   });
@@ -54,13 +52,15 @@ describe('Node utilities', () => {
     it('should return true for cursor at start of node', async () => {
       const doc = traakBuilders.doc(traakBuilders.line('<a>Hello from traak'));
       const view = createView(createState(doc));
-      const result = isCursorAtStartOfNode(view);
+      const manager = new TraakManager(view);
+      const result = manager.isCursorAtStartOfNode();
       expect(result).toEqual(true);
     });
     it('should return false for cursor at middle of node', async () => {
       const doc = traakBuilders.doc(traakBuilders.line('Hello <a> from traak'));
       const view = createView(createState(doc));
-      const result = isCursorAtStartOfNode(view);
+      const manager = new TraakManager(view);
+      const result = manager.isCursorAtStartOfNode();
       expect(result).toEqual(false);
     });
   });
@@ -75,7 +75,8 @@ describe('Node utilities', () => {
         traakBuilders.line('Hellofrom traak<a>'),
         traakBuilders.line(''),
       );
-      moveCursorToEndOfNode(view);
+      const manager = new TraakManager(view);
+      manager.moveCursorToEndOfNode();
       ist(view.state.doc, expectedDoc, eq);
     });
   });
@@ -83,7 +84,8 @@ describe('Node utilities', () => {
     it('should add a node at the current selection', async () => {
       const doc = traakBuilders.doc(traakBuilders.line('Hello from traak<a>'));
       const view = createView(createState(doc));
-      addNode(view, traakSchema.nodes['line']);
+      const manager = new TraakManager(view);
+      manager.addNode(traakSchema.nodes['line']);
       const expectedDoc = traakBuilders.doc(
         traakBuilders.line('Hello from traak'),
         traakBuilders.line(),
