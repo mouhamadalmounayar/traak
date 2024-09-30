41ba3f7d1c2495f3020c69d6a5f9fdec3e472699
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-09-30 23:46:15 +0200

    refactor(builtins): modify abstraction in TraakPlugin type

--- projects/traak-editor/src/lib/traakPlugins/TraakPlugin.ts
+++ projects/traak-editor/src/lib/traakPlugins/TraakPlugin.ts
@@ -1,12 +1,23 @@
-import { Directive } from '@angular/core';
 import { Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Coordinates } from '../../types/traakConfiguration';
-@Directive()
+import { TraakManager } from '../utilities';
+
+/**
+ * An abstract that defines the methods and attributes to have a functional plugin.
+ */
 export abstract class TraakPlugin {
-  abstract view?: EditorView;
-  abstract currentTransaction?: Transaction;
-  abstract coordinates?: Coordinates;
-  abstract isPluginVisible: boolean;
+  view?: EditorView;
+  currentTransaction?: Transaction;
+  coordinates?: Coordinates;
+  isPluginVisible: boolean;
+  manager: TraakManager;
   abstract updatePlugin(): void;
+  constructor() {
+    this.view = null;
+    this.currentTransaction = null;
+    this.coordinates = null;
+    this.isPluginVisible = false;
+    this.manager = new TraakManager(this.view);
+  }
 }
