2e2f5db3d7120ab874a4890a8d007998e91ffc17
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-10-08 20:54:59 +0200

    feat(builtins): update base schema with marks

--- projects/traak-editor/src/lib/builtins/schemas/index.ts
+++ projects/traak-editor/src/lib/builtins/schemas/index.ts
@@ -155,6 +155,50 @@ export const baseSchema = new Schema({
       content: 'block*',
     },
   },
+  marks: {
+    bold: {
+      toDOM() {
+        return ['strong', 0];
+      },
+      parseDOM: [{ tag: 'strong' }],
+    },
+    italic: {
+      toDOM() {
+        return ['i', 0];
+      },
+      parseDOM: [{ tag: 'i' }],
+    },
+    strikethrough: {
+      toDOM() {
+        return ['s', 0];
+      },
+      parseDOM: [{ tag: 's' }],
+    },
+    code: {
+      toDOM() {
+        return ['code', 0];
+      },
+      parseDOM: [{ tag: 'code' }],
+    },
+    link: {
+      attrs: {
+        href: {},
+      },
+      inclusive: false,
+      toDOM(node) {
+        const { href } = node.attrs;
+        return ['a', { href }, 0];
+      },
+      parseDOM: [
+        {
+          tag: 'a[href]',
+          getAttrs(dom) {
+            return { href: dom.getAttribute('href') };
+          },
+        },
+      ],
+    },
+  },
 });
 
 export const createSchema = (schema: Schema, type: string, node: NodeSpec) => {
